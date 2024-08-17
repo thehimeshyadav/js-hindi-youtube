95c5941992ead9ee41e8a869eadbdd6e33d61606
thehimeshyadav <thehimesh2511@gmail.com>
2024-08-17 05:40:05 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert ( 3 + 3) //we are using nodejs, not browser
+
+console.log (3
+     +
+      3) //code readability should be high
+
+console.log("Himesh")
+
+
+let name = "himesh"
+let age = 18
+let isLoggedIn = false
+let states;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/fasle
+// null => standlone value
+// undefined => 
+// symbol =>unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
