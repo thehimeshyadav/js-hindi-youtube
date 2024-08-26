79b15d4cf529e1478edff0c76df00476995e95ad
thehimeshyadav <thehimesh2511@gmail.com>
2024-08-26 06:10:50 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+// primitive
+
+// 7 types : String, Number, Boolearn, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman,", "naagraj", "doga"];
+let myObj = {
+    name: "himesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log( typeof anotherId);
